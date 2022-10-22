69d2d5629a342c3e783f5040aee296aab0f68308
jose <montesotero8@gmail.com>
2022-10-22 18:00:09 -0500

    clase03 terminada

--- script.js
+++ script.js
@@ -3,8 +3,31 @@ const buton = document.querySelector("[data-form-btn]");
 //** arrow function */
 const createTask = (event) =>{
     event.preventDefault();
+
     const input = document.querySelector("[data-form-input]");
-    console.log(input.value);
+    const value = input.value
+    const list = document.querySelector("[data-list]")
+    const task = document.createElement("li")
+    task.classList.add("card")
+    input.value = ""
+
+    console.log(value)
+    console.log(task.value)
+
+
+    //*? backticks  permite agregar codigo html */
+    const content = `<div>
+    <i class="far fa-check-square icon"></i>
+    <span class="task" >${value}</span>
+    </div>
+    <i class="fas fa-trash-alt trashIcon icon"></i>`
+    console.log(content)
+
+  //*? agregar a html usamos la propiedad innerHtml */
+    task.innerHTML = content
+
+  //** agregar mos un hijo al html usamos apprndChild(hijo en html)*/
+    list.appendChild(task)
 }
 
 buton.addEventListener("click", createTask)
